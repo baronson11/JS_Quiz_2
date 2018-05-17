4366a112d5eb626a95416985162b98bcd54e809d
smibbie <35998407+smibbie@users.noreply.github.com>
2018-05-17 18:30:34 -0400

    edits made to reflect ES6

--- js/quiz.js
+++ js/quiz.js
@@ -1,22 +1,23 @@
-var questions = [
+const questions = [
   ['How many states are in the United States?', 50],
   ['How many continents are there?', 7],
-  ['How many legs does an insect have?', 6]
+  ['How many oceans are there?', 4]
 ];
 
-var correct = [];
-var incorrect = [];
+let correct = [];
+let incorrect = [];
 
-var correctAnswers = 0;
-var answer;
-var response;
+let correctAnswers = 0;
+let answer;
+let response;
+let html;
 
 function print(message) {
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
 }
 
-for (var i = 0; i < questions.length; i += 1) {
+for (let i = 0; i < questions.length; i += 1) {
   response = parseInt(prompt(questions[i][0]));
   answer = questions[i][1];
   if (response === answer) {
@@ -27,4 +28,11 @@ for (var i = 0; i < questions.length; i += 1) {
   }
 }
 
-print("You got " + correctAnswers + " question(s) right." + "<br><br>" + "Here's what you got right: <br>" + "<ul><li>" + correct + "</li></ul>" + "<br> Here's what you got wrong: <br>" + "<ul><li>" + incorrect + "</li></ul>");
+html = `<ul><li>You got ${correctAnswers} question(s) right.</li>
+        <li>Here's what you got right:</li>
+        <li>${correct}</li>
+        <li>Heres what you got wrong:</li>
+        <li>${incorrect}</li>
+        </ul>`
+
+print(html);
